556158968b37bd7fd76e84c7843e95053826202e
burakyilmazdev <burakyilmaz1627@gmail.com>
2020-12-22 00:18:42 +0300

    Constructor injection has been done.

---
+++ app/src/main/java/com/example/dagger2/Computer.java
@@ -0,0 +1,21 @@
+package com.example.dagger2;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Computer {
+    private static final String TAG = "Computer";
+    RAM ram;
+    Processor processor;
+
+    @Inject
+    public Computer(RAM ram, Processor processor) {
+        this.ram = ram;
+        this.processor = processor;
+    }
+
+    public void work(){
+        Log.d(TAG, "Computer is working ");
+    }
+}
--- app/src/main/java/com/example/dagger2/MainActivity.java
+++ app/src/main/java/com/example/dagger2/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Computer computer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ComputerComponent computerComponent = DaggerComputerComponent.create();
+        computer = computerComponent.getComputer();
+        computer.work();
+
     }
 }
